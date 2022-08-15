f3a39b1420b0753f2defd55b1aaedc01423eee91
TerryFresh <smile-1994@mail.ru>
2022-08-16 02:40:39 +0300

    Add Tusk1 and Tusk2

---
+++ src/Main1.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+//У нас есть справочник городов. Он представлен в текстовом виде и состоит из следующих полей:
+//Порядковый номер записи справочника;
+//Наименование города;
+//Регион;
+//Федеральный округ;
+//Количество жителей;
+//Дата основания или первое упоминание
+
+//Необходимо реализовать класс Main, используя java.util.Scanner, прочесть информацию из текстового представления справочника и разложить данные в модель City с полями:
+//name – наименование города
+//region – регион
+//district – федеральный округ
+//population – количество жителей города
+//foundation – дата основания или первое упоминание
+
+//Пример полученного результата:
+//City{name='Адыгейск', region='Адыгея', district='Южный', population=12248, foundation='1973'}
+//City{name='Майкоп', region='Адыгея', district='Южный', population=144246, foundation='1857'}
+//City{name='Горно-Алтайск', region='Алтай', district='Сибирский', population=56928, foundation='1830'}
+
+public class Main1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(
+                "D:\\Java\\Задача ВС Java Сбер.csv"));
+
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+        ArrayList<City> cities = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+            City city = new City();
+            scanner = new Scanner(line);
+            scanner.useDelimiter(";");
+            while (scanner.hasNext()) {
+                String data = scanner.next();
+                if (index == 0)
+                    city.setId(Integer.parseInt(data));
+                else if (index == 1)
+                    city.setName(data);
+                else if (index == 2)
+                    city.setRegion(data);
+                else if (index == 3)
+                    city.setDistrict(data);
+                else if (index == 4)
+                    city.setPopulation(Long.parseLong(data));
+                else if (index == 5)
+                    city.setFoundation(data);
+                index++;
+            }
+            index = 0;
+            cities.add(city);
+        }
+        reader.close();
+
+        System.out.println(cities);
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+//Теперь нам нужно реализовать несколько вариантов сортировки данных справочника:
+//Сортировка списка городов по наименованию в алфавитном порядке по убыванию без учета регистра;
+//Сортировка списка городов по федеральному округу и наименованию города внутри каждого федерального округа в алфавитном порядке по убыванию с учетом регистра;
+//Затем вывести полученный список объектов City в консоль. При реализации сортировки нужно обратить внимание на возможные варианты реализации: Comparator, lambda-выражения.
+//Пример полученного результата для сортировки по наименованию:
+
+//…Сортировка списка городов по наименованию в алфавитном порядке по убыванию без учета регистра
+//City{name='Абаза', region='Хакасия', district='Сибирский', population=17111, foundation='1867'}
+//City{name='Абакан', region='Хакасия', district='Сибирский', population=165183, foundation='1931'}
+//City{name='Абдулино', region='Оренбургская область', district='Приволжский', population=20663, foundation='1795'}
+
+//…Сортировка списка городов по федеральному
+//Пример полученного результата для сортировки по двум полям справочника – федеральному округу и наименованию города:
+//City{name='Алдан', region='Якутия', district='Дальневосточный', population=21277, foundation='1924'}
+//City{name='Александровск-Сахалинский', region='Сахалинская область', district='Дальневосточный', population=10613, foundation='1869'}
+//City{name='Амурск', region='Хабаровский край', district='Дальневосточный', population=42977, foundation='1958'}
+
+//…Наименованию города внутри каждого федерального округа в алфавитном порядке по убыванию с учетом регистра
+//City{name='Абдулино', region='Оренбургская область', district='Приволжский', population=20663, foundation='1795'}
+//City{name='Агидель', region='Башкортостан', district='Приволжский', population=16365, foundation='1980'}
+//City{name='Агрыз', region='Татарстан', district='Приволжский', population=19299, foundation='1646'}
+
+public class Main2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(
+                "D:\\Java\\Задача ВС Java Сбер.csv"));
+
+        String line = null;
+        Scanner scanner = null;
+        int index = 0;
+        ArrayList<City> cities = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+            City city = new City();
+            scanner = new Scanner(line);
+            scanner.useDelimiter(";");
+            while (scanner.hasNext()) {
+                String data = scanner.next();
+                if (index == 0)
+                    city.setId(Integer.parseInt(data));
+                else if (index == 1)
+                    city.setName(data);
+                else if (index == 2)
+                    city.setRegion(data);
+                else if (index == 3)
+                    city.setDistrict(data);
+                else if (index == 4)
+                    city.setPopulation(Long.parseLong(data));
+                else if (index == 5)
+                    city.setFoundation(data);
+                index++;
+            }
+            index = 0;
+            cities.add(city);
+        }
+        reader.close();
+
+//        System.out.println(cities);
+//        sortCityByName(cities);
+//        sortCityByNameAndDistrict(cities);
+    }
+
+    public static void sortCityByName(ArrayList cities){
+        Collections.sort(cities, (Comparator<City>) (o1, o2) -> o1.getName().toLowerCase(Locale.ROOT).compareTo(o2.getName().toLowerCase()));
+        for (Object city : cities){
+            System.out.print(city);
+        }
+    }
+
+    public static void sortCityByNameAndDistrict(ArrayList cities){
+        Collections.sort(cities, (Comparator<City>) (o1, o2) ->
+        {
+            int secondSort = o1.getDistrict().compareTo(o2.getDistrict());
+            if (secondSort != 0)
+                return secondSort;
+            return o1.getName().compareTo(o2.getName());
+        });
+        for (Object city : cities){
+            System.out.print(city);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
